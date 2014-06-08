ccf1d96a6bbd90a62d260970b99ab83b7bcfbd62
Nick Cox <nickcox1008@gmail.com>
2014-06-08 12:10:14 -0600

    more options in the demo

--- demo.js
+++ demo.js
@@ -11,28 +11,39 @@ function webGLStart() {
 	window.addEventListener('resize', resize);
 	
 	var axis = new Axis(10);
-	var sphere = new Sphere(5, 36, 36);
-	var earthAngle = dtor(-180);
-	var earthSpeed = dtor(35) / 1000;
+	var angle = dtor(-180);
+	var speed = dtor(35) / 1000;
+	var shape = document.getElementById('shape');
+	var shapes = {
+		'sphere': new Sphere(5, 36, 36),
+		'cylinder': new Cylinder(4, 8, 36, true)
+	};
+	var rotate = document.getElementById('rotate');
+	var texture = document.getElementById('texture');
 	var textures = {
 		'earth': gl.buildTexture('earth.jpg'),
 		'galvanized': gl.buildTexture('galvanized.jpg')
 	};
 	
-/*	gl.animationHandlers.push(function(elapsed) {
-		earthAngle += earthSpeed * elapsed;
-	});*/
+	gl.animationHandlers.push(function(elapsed) {
+		if (rotate.checked) {
+			angle += speed * elapsed;
+		}
+		return rotate.checked;
+	});
 	
 	gl.drawHandlers.push(function() {
 		gl.useTextures(false);
 		gl.materialShininess(0.0);
 		axis.draw();
 		
-		gl.setTexture(textures[document.getElementById('texture').value]);
+		gl.setTexture(textures[texture.value]);
 		gl.materialShininess(32.0);
-		gl.rotate(earthAngle, [0, 0, 1]);
-			sphere.draw();
-		gl.rotate(-earthAngle, [0, 0, 1]);
+		gl.rotate(angle, [0, 0, 1]);
+			if (shapes[shape.value]) {
+				shapes[shape.value].draw();
+			}
+		gl.rotate(-angle, [0, 0, 1]);
 	});
 	
 	gl.tick();
